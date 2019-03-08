3bed8cefd2fe2641c490239db664f16cee9166ad
Vitalii Kulyk <vitcool@ukr.net>
2019-03-08 20:31:26 +0200

    [HRA-3] Mutations: Creating Links

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react'
-import LinkList from './LinkList'
+import React, { Component } from 'react';
+import CreateLink from './CreateLink';
 
 class App extends Component {
   render() {
-    return <LinkList />
+    return <CreateLink />;
   }
 }
 
-export default App
+export default App;
